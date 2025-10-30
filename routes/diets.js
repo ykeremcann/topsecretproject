3f2b51d7d9cb7c7e383d1437b14cebed33a4ae0c
CarnaHouse <gorkempasaoglu@outlook.com>
2025-10-30 16:35:54 +0300

    index.js updated

--- routes/diets.js
+++ routes/diets.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const dietController = require('../controllers/dietController');
-const auth = require('../middleware/auth');
+const dietController = require("../controllers/dietController");
+const auth = require("../middleware/auth");
 
 // All routes require authentication
 router.use(auth);
 
-router.get('/', dietController.getDiets);
-router.get('/stats', dietController.getStats);
-router.post('/', dietController.createDiet);
-router.put('/:id', dietController.updateDiet);
-router.delete('/:id', dietController.deleteDiet);
-router.post('/:id/complete', dietController.completeDiet);
-router.patch('/:id/toggle', dietController.toggleDiet);
+router.get("/", dietController.getDiets);
+router.get("/stats", dietController.getStats);
+router.post("/", dietController.createDiet);
+router.put("/:id", dietController.updateDiet);
+router.delete("/:id", dietController.deleteDiet);
+router.post("/:id/complete", dietController.completeDiet);
+router.patch("/:id/toggle", dietController.toggleDiet);
 
 module.exports = router;
